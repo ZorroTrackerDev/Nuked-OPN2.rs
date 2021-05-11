510b0c51cfc61f113fa6e51c3251c8d0a3acb638
ShinySaana <shinysaana@gmail.com>
2021-05-11 16:52:56 +0200

    fix unexistant typo

--- nuked-opn2-node/scripts/build-linux-x64.sh
+++ nuked-opn2-node/scripts/build-linux-x64.sh
@@ -4,7 +4,5 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-rustup target add x86_64-unknown-linux-gnu
-
 $SCRIPT_DIR/build.sh build:linux:x64 linux-x64 x86_64-unknown-linux-gnu/release/libnuked_opn2_node.so
 
--- nuked-opn2-node/scripts/build-linux-x86.sh
+++ nuked-opn2-node/scripts/build-linux-x86.sh
@@ -4,6 +4,4 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-rustup target add i686-unknown-linux-gnu
-
 $SCRIPT_DIR/build.sh build:linux:x86 linux-x86 i686-unknown-linux-gnu/release/libnuked_opn2_node.so
